ed67720e21bb8726efad4ea885b762c905e84ed3
Webhead1104 <webhead1104@hotmail.com>
2026-01-23 16:11:07 -0600

    Use blossom for paper GeneratedClassloader
    Signed-off-by: Webhead1104 <webhead1104@hotmail.com>

--- platforms/paper/build.gradle.kts
+++ platforms/paper/build.gradle.kts
@@ -4,8 +4,9 @@ plugins {
     id("xyz.jpenilla.run-paper") version "3.0.2"
     id("com.gradleup.shadow") version "9.3.1"
     id("me.modmuss50.mod-publish-plugin") version "1.1.0"
+    id("net.kyori.blossom") version "2.2.0"
 }
-version = project.findProperty("plugin_version") as String? ?: "unknown"
+version = project.findProperty("plugin_version") as String
 val minecraftVersion = findProperty("minecraft_version") as String
 
 repositories {
@@ -40,74 +41,36 @@ dependencies {
     testRuntimeOnly(libs.junit.launcher)
 }
 
-val generateClassloader: TaskProvider<Task> = tasks.register("generateClassloader") {
-    val outputDir: File = file("$projectDir/build/generated/sources/classloader")
-    val packageDir = File(outputDir, "me/webhead1104/towncraft/utils")
-    val classloaderFile = File(packageDir, "GeneratedClassloader.java")
+sourceSets {
+    main {
+        blossom {
+            javaSources {
+                val deps = provider {
+                    val commonDeps = project(":platforms:common")
+                        .configurations.getByName("api")
+                        .dependencies
+                        .filterIsInstance<ModuleDependency>()
+                        .filter { it.group != "org.spongepowered" }
 
-    val deps = provider {
-        val commonDeps = project(":platforms:common")
-            .configurations.getByName("api")
-            .dependencies
-            .filterIsInstance<ModuleDependency>()
-            .filter { it.group != "org.spongepowered" }
+                    val platformDeps = configurations.getByName("compileOnly")
+                        .dependencies
+                        .filterIsInstance<ModuleDependency>()
+                        .filter { it.group != "io.papermc.paper" && it.group != "me.webhead1104" }
 
-        val platformDeps = configurations.getByName("compileOnly")
-            .dependencies
-            .filterIsInstance<ModuleDependency>()
-            .filter { it.group != "io.papermc.paper" && it.group != "me.webhead1104" }
-
-        (commonDeps + platformDeps).map { "${it.group}:${it.name}:${it.version}" }
-    }
-
-    inputs.property("dependencies", deps)
-    outputs.file(classloaderFile)
-
-    doLast {
-        packageDir.mkdirs()
-        outputDir.mkdirs()
-        classloaderFile.createNewFile()
-
-        val depsString: String = deps.get().joinToString("\n                    ") {
-            "resolver.addDependency(new Dependency(new DefaultArtifact(\"$it\"), null));"
-        }
-
-        classloaderFile.writeText(
-            """
-            package me.webhead1104.towncraft.utils;
-            
-            import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
-            import io.papermc.paper.plugin.loader.PluginLoader;
-            import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
-            import org.eclipse.aether.artifact.DefaultArtifact;
-            import org.eclipse.aether.graph.Dependency;
-            import org.eclipse.aether.repository.RemoteRepository;
-            import org.jetbrains.annotations.NotNull;
-
-            public class GeneratedClassloader implements PluginLoader {
-
-                @Override
-                public void classloader(@NotNull PluginClasspathBuilder classpathBuilder) {
-                    System.setProperty("bstats.relocatecheck", "false");
-                    MavenLibraryResolver resolver = new MavenLibraryResolver();
-                    resolver.addRepository(new RemoteRepository.Builder("geyser", "default", "https://repo.opencollab.dev/maven-snapshots").build());
-                    resolver.addRepository(new RemoteRepository.Builder("central", "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR).build());
-                    
-                    $depsString
-
-                    classpathBuilder.addLibrary(resolver);
+                    (commonDeps + platformDeps).map { "${it.group}:${it.name}:${it.version}" }
                 }
+                val depsString: String = deps.get().joinToString(separator = "\n        ") {
+                    """resolver.addDependency(new Dependency(new DefaultArtifact("${
+                        it.replace(
+                            ".",
+                            "$"
+                        )
+                    }".replace("$", ".")), null));"""
+                }.trimMargin()
+                properties.put("depString", depsString)
             }
-            """.trimIndent()
-        )
-
-        println("Generated GeneratedClassloader.java at $classloaderFile")
+        }
     }
-}
-
-tasks.compileJava {
-    dependsOn(generateClassloader)
-    source(generateClassloader.map { layout.buildDirectory.dir("generated/sources/classloader").get() })
 }
 
 tasks {
