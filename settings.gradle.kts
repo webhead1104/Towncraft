05bbd5953fbf39939a6382e829567984ad1a0f85
Webhead1104 <webhead1104@hotmail.com>
2026-04-20 13:48:34 -0500

    remove: menu designer
    Signed-off-by: Webhead1104 <webhead1104@hotmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-include("tools:wiki-scraper", "tools:menu-designer")
+include("tools:wiki-scraper")
 include(":platforms:common")
 include(":platforms:cytosis")
 include(":platforms:paper")
